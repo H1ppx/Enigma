9ee289ab45b00e25b3ccb097a3c6713cb4d4d733
Will Chu <willcty@gmail.com>
2019-04-07 12:10:23 -0400

    Working with rotation now
    Tested forward and backwords

--- Python/Components/Rotor.py
+++ Python/Components/Rotor.py
@@ -1,17 +1,19 @@
 class Rotor:
 
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    amountOfRotations = 0
 
-    def __init__(self, permutation, position):
+    def __init__(self, permutation, rotation):
         self.permutation = permutation
-        self.position = position
+        self.rotation = rotation
 
     def calc(self, c, reflected):
         if reflected:
-            return self.alphabet[self.permutation.find(c)]
+            return self.alphabet[(self.permutation.find(c) + self.rotation) % 26]
         else:
-            return self.permutation[self.alphabet.find(c)]
+            return self.permutation[(self.alphabet.find(c) - self.rotation) % 26]
 
     def rotate(self):
-        self.shifts
-        # TODO
+        self.rotation += 1
+        self.amountOfRotations += 1
+        return self.amountOfRotations % 26
--- Python/Enigma.py
+++ Python/Enigma.py
@@ -5,50 +5,65 @@ from Python.Components.Reflector import Reflector
 
 class Enigma:
 
-    def __init__(self, rotorAlphabetL, rotorAlphabetM, rotorAlphabetR):
+    def __init__(self, rotorAlphabetFast, rotorAlphabetMed, rotorAlphabetSlow):
         self.plugboard = Plugboard()
-        self.rotorSlow = Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO",1)
-        self.rotorMed = Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE",1)
-        self.rotorFast = Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ",1)
+        self.rotorFast = Rotor(rotorAlphabetFast, 0)
+        self.rotorMed = Rotor(rotorAlphabetMed, 0)
+        self.rotorSlow = Rotor(rotorAlphabetSlow, 0)
         self.reflector = Reflector()
 
     def run(self, text):
         text = text.upper()
         output = ""
         for c in text:
-            temp = ""
+            temp = c
+
             # Plugboard 1st Calc
-            temp = self.plugboard.calc(c)
+            temp = self.plugboard.calc(temp)
+            # print(temp)
 
             # Fast Rotor 1st Calc
             temp = self.rotorFast.calc(temp, False)
+            # print(temp)
 
             # Med Rotor 1st Calc
             temp = self.rotorMed.calc(temp, False)
+            # print(temp)
 
             # Slow Rotor 1st Calc
             temp = self.rotorSlow.calc(temp, False)
+            # print(temp)
 
             # Reflector
             temp = self.reflector.calc(temp)
+            # print(temp)
 
             # Slow Rotor 2nd Calc
             temp = self.rotorSlow.calc(temp, True)
+            # print(temp)
 
             # Med Rotor 1st Calc
             temp = self.rotorMed.calc(temp, True)
+            # print(temp)
 
             # Fast Rotor 1st Calc
             temp = self.rotorFast.calc(temp, True)
+            # print(temp)
 
             # Plugboard 2nd Calc
             temp = self.plugboard.calc(temp)
+            # print(temp)
 
             # Add to Output
             output += temp
 
+            # Rotate Motor
+            if self.rotorFast.rotate() == 0:
+                if self.rotorMed.rotate() == 0:
+                    self.rotorSlow.rotate()
+
         return output
 
 
-test = Enigma("abc", "def", "ghi")
+test = Enigma("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO")
 print(test.run(input("Text \n")))
