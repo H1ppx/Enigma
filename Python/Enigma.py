2f29e735e3b7a9acb9f866b1c246da9f157cba6c
Will Chu <willcty@gmail.com>
2019-04-07 14:49:10 -0400

    Update readme and add license

--- Python/Enigma.py
+++ Python/Enigma.py
@@ -5,54 +5,52 @@ from Python.Components.Reflector import Reflector
 
 class Enigma:
 
-    def __init__(self, rotorAlphabetFast, rotorAlphabetMed, rotorAlphabetSlow):
+    def __init__(self, rotorAlphabetFast, rotorRotationFast, rotorAlphabetMed, rotorRotationMed, rotorAlphabetSlow, rotorRotationSlow):
         self.plugboard = Plugboard()
-        self.rotorFast = Rotor(rotorAlphabetFast, 0)
-        self.rotorMed = Rotor(rotorAlphabetMed, 0)
-        self.rotorSlow = Rotor(rotorAlphabetSlow, 0)
+        self.rotorFast = Rotor(rotorAlphabetFast, rotorRotationFast)
+        self.rotorMed = Rotor(rotorAlphabetMed, rotorRotationMed)
+        self.rotorSlow = Rotor(rotorAlphabetSlow, rotorRotationSlow)
         self.reflector = Reflector()
+
+    def printDayKey(self):
+        print("Rotor Fast Alphabet: " + self.rotorFast.permutation)
+        print("Rotor Med Alphabet: " + self.rotorMed.permutation)
+        print("Rotor Slow Alphabet: " + self.rotorSlow.permutation)
+        print("Plugboard: " + ''.join(self.plugboard.key1) + ", " + ''.join(self.plugboard.key2))
 
     def run(self, text):
         text = text.upper()
+        text = text.replace(" ", "")
         output = ""
         for c in text:
             temp = c
 
             # Plugboard 1st Calc
             temp = self.plugboard.calc(temp)
-            # print(temp)
 
             # Fast Rotor 1st Calc
             temp = self.rotorFast.calc(temp, False)
-            # print(temp)
 
             # Med Rotor 1st Calc
             temp = self.rotorMed.calc(temp, False)
-            # print(temp)
 
             # Slow Rotor 1st Calc
             temp = self.rotorSlow.calc(temp, False)
-            # print(temp)
 
             # Reflector
             temp = self.reflector.calc(temp)
-            # print(temp)
-
+            
             # Slow Rotor 2nd Calc
             temp = self.rotorSlow.calc(temp, True)
-            # print(temp)
 
             # Med Rotor 1st Calc
             temp = self.rotorMed.calc(temp, True)
-            # print(temp)
 
             # Fast Rotor 1st Calc
             temp = self.rotorFast.calc(temp, True)
-            # print(temp)
 
             # Plugboard 2nd Calc
             temp = self.plugboard.calc(temp)
-            # print(temp)
 
             # Add to Output
             output += temp
@@ -65,5 +63,7 @@ class Enigma:
         return output
 
 
-test = Enigma("EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO")
-print(test.run(input("Text \n")))
+test = Enigma("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 0, "AJDKSIRUXBLHWTMCQGZNPYFVOE", 0, "BDFHJLCPRTXVZNYEIWGAKMUSQO", 0)
+# print(test.run(input("Enter Text: \n")))
+test.printDayKey()
+
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Enigma
+
+//TODO
+
+## Getting Started
+
+//TODO
+
+### Prerequisites
+
+//TODO
+
+### How to Use
+
+//TODO
+
+## License
+
+This project is licensed under the Apache 2.0 License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Dr. David Perry, National Security Agency
